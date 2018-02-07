6daf01712e1541173ee8bbac4917fbf841d0825a
Noel Manuel <setsuna04@gmail.com>
2018-02-07 19:13:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {array.unshif
 function addElementToEndOfArray(array , element) {return [...array, element];}
 
 function destructivelyAddElementToEndOfArray(array, element) {array.push(element); return array;}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
