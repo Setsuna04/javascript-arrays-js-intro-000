28818f3583429d52d7a5a9f92c1e136e801a2c6c
Noel Manuel <setsuna04@gmail.com>
2018-02-07 19:10:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
 ];
 
-var addElementToBeginningOfArray = (array, element) => {return [element, ...array]; }
+function addElementToBeginningOfArray(array, element) {return [element, ...array]; }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element);}
 
