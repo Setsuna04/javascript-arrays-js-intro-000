7a322c7c44ec56b6d551548f7ed899cc5ca27883
Noel Manuel <setsuna04@gmail.com>
2018-02-07 19:11:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,3 +10,5 @@ function addElementToBeginningOfArray(array, element) {return [element, ...array
 function destructivelyAddElementToBeginningOfArray(array, element) {array.unshift(element);}
 
 function addElementToEndOfArray(array , element) {return [...array, element];}
+
+function destructivelyAddElementToEndOfArray(array, element) {array.push(element);}
